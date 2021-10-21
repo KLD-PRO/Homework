08279231892ff5b82a743be07b6f0e52c5dbdb17
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-10-21 10:01:44 +0200

    task1 done

--- practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp2.java
+++ practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp2.java
@@ -2,6 +2,19 @@ package ru.gb.KLD_PRO;
 
 public class HomeWorkApp2 {
     public static void main(String[] args) {
+        TwoNumbers(); {
+            System.out.println(TwoNumbers());
+        }
 
+    }
+    public static boolean TwoNumbers () {
+        int a = 13, b = 37;
+        int c = a + b;
+        if (10 >= c || c <= 20) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
