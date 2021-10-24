799ee219feb1d3c326275037dda11525dafc222b
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-10-24 21:48:48 +0200

    task1 done

--- practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp3.java
+++ practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp3.java
@@ -2,6 +2,15 @@ package ru.gb.KLD_PRO;
 
 public class HomeWorkApp3 {
     public static void main(String[] args) {
+        dataReplacement();
+    }
+
+    public static void dataReplacement() {
+        int [] abc = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < abc.length; i++) {
+            abc [i] = (abc [i] > 0) ? 0 : 1;
+            System.out.print(abc [i]);
+        }
 
     }
 }
