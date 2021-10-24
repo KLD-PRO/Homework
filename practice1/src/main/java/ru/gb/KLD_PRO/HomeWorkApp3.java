5f816375ee9de39ab7c9965a00eb1b49370620f2
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-10-24 22:01:45 +0200

    task2 done

--- practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp3.java
+++ practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp3.java
@@ -2,15 +2,27 @@ package ru.gb.KLD_PRO;
 
 public class HomeWorkApp3 {
     public static void main(String[] args) {
-        dataReplacement();
+        dataReplacement(); {
+
+        }
+        emptyIntegerArray();
     }
 
     public static void dataReplacement() {
         int [] abc = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < abc.length; i++) {
             abc [i] = (abc [i] > 0) ? 0 : 1;
-            System.out.print(abc [i]);
+            System.out.print(abc [i] + " ");
         }
 
+    }
+
+    public static void emptyIntegerArray() {
+        System.out.println();
+        int [] gr = new int [100];
+        for (int i = 0; i < gr.length; i++) {
+            gr [i] = i + 1;
+            System.out.print(gr [i] + " ");
+        }
     }
 }
