330e2025d97d994722494d2866158aaac670d32b
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-11-04 17:57:58 +0200

    task4 done

--- practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp5.java
+++ practice1/src/main/java/ru/gb/KLD_PRO/HomeWorkApp5.java
@@ -1,5 +1,15 @@
 package ru.gb.KLD_PRO;
 
 public class HomeWorkApp5 {
+    public static void main(String[] args) {
+        Employee [] employees {
+            new Employee("LukeSkywalker", "Jedi", "LK@mail.ru", "456-123", 100, 23),
+                    new Employee("ObiVanKenobi", "MasterJedi", "OVK@mail.ru", "456-124", 1000, 42),
+                    new Employee("MaceVindu", "MasterJedi2", "MV@mail.ru", "456-125", 10000, 51),
+                    new Employee("Yoda", "GrandMasterJedi", "Yo@mail.ru", "456-100", 100000, 200),
+                    new Employee("DartSidius", "Sith", "DarkEmperor@mail.ru", "666", 1000000, 66)
+        };
+
+    }
 
 }
