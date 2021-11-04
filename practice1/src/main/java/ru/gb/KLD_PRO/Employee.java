060aebd4d2b4ecf3fb8cfe214246fdc5a8819f29
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-11-04 17:32:22 +0200

    task2 done

--- practice1/src/main/java/ru/gb/KLD_PRO/Employee.java
+++ practice1/src/main/java/ru/gb/KLD_PRO/Employee.java
@@ -1,10 +1,19 @@
 package ru.gb.KLD_PRO;
 
 public class Employee {
-    private String FullName;
+    private String fullName;
     private String post;
     private String email;
     private String telephone;
     private int salary;
     private int age;
+
+    public Employee(String fullName, String post, String email, String telephone, int salary, int age) {
+        this.fullName = fullName;
+        this.post = post;
+        this.email = email;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.age = age;
+    }
 }
