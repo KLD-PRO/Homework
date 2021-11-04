a2b776ca379a815724de9787929a0e2073b9f3d8
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-11-04 17:12:45 +0200

    task1 done

--- practice1/src/main/java/ru/gb/KLD_PRO/Employee.java
+++ practice1/src/main/java/ru/gb/KLD_PRO/Employee.java
@@ -1,10 +1,10 @@
 package ru.gb.KLD_PRO;
 
 public class Employee {
-    String name;
-    String post;
-    String email;
-    int telephone;
-    int salary;
-    int age;
+    private String FullName;
+    private String post;
+    private String email;
+    private String telephone;
+    private int salary;
+    private int age;
 }
