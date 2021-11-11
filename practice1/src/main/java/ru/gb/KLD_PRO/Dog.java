60941b340a8bc55e601b481a91bed6fe9a611923
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-11-11 16:48:43 +0200

    dog done

---
+++ practice1/src/main/java/ru/gb/KLD_PRO/Dog.java
@@ -0,0 +1,7 @@
+package ru.gb.KLD_PRO;
+
+public class Dog extends Animal {
+        public Dog(int runDistance, int swimDistance, float jumpHeight) {
+            super(runDistance, swimDistance, jumpHeight);
+        }
+}
