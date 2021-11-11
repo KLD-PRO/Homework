df8cd31c1a1098fa321f5557f76248316831a1db
KLD_PRO <92334398+KLD-PRO@users.noreply.github.com>
2021-11-11 16:25:24 +0200

    visible pane done

---
+++ practice1/src/main/java/ru/gb/KLD_PRO/Game.java
@@ -0,0 +1,21 @@
+package ru.gb.KLD_PRO;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Game extends JFrame {
+    private MyPanel Pa;
+    private MyButtons Ex_RES;
+
+    Game() {
+        setLayout(new BorderLayout());
+        setVisible(true);
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Pa = new MyPanel();
+        Ex_RES = new MyButtons();
+        Ex_RES.SetObject(Pa);
+        add(Ex_RES, BorderLayout.EAST);
+        add(Pa, BorderLayout.CENTER);
+    }
+}
